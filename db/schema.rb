31f43a9d994809a06b2e10f9c224cdfbda7daef3
Renato Toth <renatomolinat@gmail.com>
2018-09-22 16:17:03 -0700

    making pg the default database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_09_21_011350) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "asin"
     t.string "rank"
