1e0cd7ec627e0f67ba0058a7c55a21ad538724c7
Renato Toth <renatomolinat@gmail.com>
2018-09-23 12:15:16 -0700

    adding message when the page has no product to render

--- app/assets/javascripts/components/product_list.jsx
+++ app/assets/javascripts/components/product_list.jsx
@@ -4,7 +4,7 @@ class ProductList extends React.Component {
       <div>
         <hr/>
         <h2>Products</h2>
-        {this.props.products ?
+        {this.props.products.length !== 0 ?
           this.props.products.map((product) => <ProductItem product={product}/>)
           : <span>There are no products registered.</span>}
       </div>
