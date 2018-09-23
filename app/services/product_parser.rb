a8ad7ca847b2c18db3b3e9df7d828ae78a3b482b
Renato Toth <renatomolinat@gmail.com>
2018-09-23 00:23:44 -0700

    refactoring product parser

--- app/services/product_parser.rb
+++ app/services/product_parser.rb
@@ -1,16 +1,14 @@
 class ProductParser
-  def self.build_product(asin)
+  def self.build(asin)
     begin
       page_content = open("https://www.amazon.com/dp/#{asin}").read
 
-      product = Product.new(
+      Product.new(
         asin: asin,
-        dimensions: parse_product_dimensions(page_content),
-        category: parse_category(page_content),
-        rank: parse_rank(page_content)
+        dimensions: dimension_parser(page_content),
+        category: category_parser(page_content),
+        rank: rank_parser(page_content)
       )
-
-      product
     rescue
       return nil
     end
@@ -18,24 +16,26 @@ class ProductParser
 
   private
 
-  def self.parse_product_dimensions(page_content)
+  def self.dimension_parser(page_content)
     page_content[/\d+((\d+|\.)\d+)?\sx\s\d+((\d+|\.)\d+)?\sx\s\d+((\d+|\.)\d+)?\sinches/] || 
       "Not found"
   end
 
-  def self.parse_category(page_content)
-    rank_category_element_text = rank_category_element(page_content)
-    return 'Not found' if rank_category_element_text.nil?
-    rank_category_element_text[/(?<= in ).*/]
+  def self.category_parser(page_content)
+    rank_category_parser_content = rank_category_parser(page_content)
+    return 'Not found' if rank_category_parser_content.nil?
+    rank_category_parser_content[/(?<= in ).*/]
   end
 
-  def self.parse_rank(page_content)
-    rank_category_element_text = rank_category_element(page_content)
-    return 'Not found' if rank_category_element_text.nil?
-    rank_category_element_text[/\d+(\,\d+)?/]
+  def self.rank_parser(page_content)
+    rank_category_parser_content = rank_category_parser(page_content)
+    return 'Not found' if rank_category_parser_content.nil?
+    rank_category_parser_content[/\d+(\,\d+)?/]
   end
 
-  def self.rank_category_element(page_content)
+  
+  def self.rank_category_parser(page_content)
+    # Matches the following format "#12,678 in Baby"
     page_content[/\#(\d+(\,\d+)?)\sin\s\w+[^(]*/]
   end
 end
