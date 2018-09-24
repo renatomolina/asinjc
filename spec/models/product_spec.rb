5ab0fe92bd08c3e0c21608d4ba500f8bd941e13d
Renato Toth <renatomolinat@gmail.com>
2018-09-24 09:56:07 -0700

    removing unenecessary code

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'basic validations' do
-    subject { Product.new }
-
     it { is_expected.to validate_presence_of(:asin) }
     it { is_expected.to validate_uniqueness_of(:asin) }
   end
